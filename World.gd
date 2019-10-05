ad280ce42b81b9f1dacd067289a38fd103dc0eff
Tiago Lisboa <tiago.caio.ol@gmail.com>
2019-10-05 11:54:55 -0300

    dungeon generator

---
+++ World.gd
@@ -0,0 +1,218 @@
+extends Node2D
+
+var Room = preload("res://Room.tscn")
+
+var tile_size = 32
+var num_rooms = 50
+var min_size = 4
+var max_size = 10
+var hspread = 400
+var cull = .5
+
+onready var Map = $TileMap
+
+var path # AStar pathfinding obj
+func _ready():
+	randomize()
+	make_rooms()
+	
+func make_rooms():
+	for i in range(num_rooms):
+		var pos = Vector2(rand_range(-hspread, hspread), 0)
+		var r = Room.instance()
+		var w = min_size + randi() % (max_size - min_size)
+		var h = min_size + randi() % (max_size - min_size)
+		r.make_room(pos, Vector2(w, h) * tile_size)
+		$Rooms.add_child(r)	
+	# wait for movement to stop
+	yield(get_tree().create_timer(1.1), 'timeout')
+	# cull rooms
+	var room_positions = []
+	for room in $Rooms.get_children():
+		if randf() < cull:
+			room.queue_free()
+		else:
+			room.mode = RigidBody2D.MODE_STATIC
+			room_positions.append(Vector3(room.position.x,
+										room.position.y, 0))
+	yield(get_tree(), 'idle_frame')
+	# generate mst
+	path = find_mst(room_positions)
+		
+func _draw():
+	for room in $Rooms.get_children():
+		draw_rect(Rect2(room.position - room.size, room.size * 2),
+				Color(32, 228, 0), false)
+	if path:
+		for p in path.get_points():
+			for c in path.get_point_connections(p):
+				var pp = path.get_point_position(p)
+				var cp = path.get_point_position(c)
+				draw_line(Vector2(pp.x, pp.y), Vector2(cp.x, cp.y),
+						Color(1, 1, 0), 15, true)
+	
+func _process(delta):
+	update()
+	
+func _input(event):
+	if event.is_action_pressed('ui_select'):
+		for n in $Rooms.get_children():
+			n.queue_free()
+		path = null
+		make_rooms()
+	if event.is_action_pressed("ui_focus_next"):
+		make_map()
+
+func find_mst(nodes):
+	# Prim's algorithm
+	var path = AStar.new()
+	path.add_point(path.get_available_point_id(), nodes.pop_front())
+	
+	# repeat until no more nodes to add
+	while nodes:
+		var min_dist = INF # minimum distance so far
+		var min_p = null # position of that node
+		var p = null # current node
+		# loop through points in path
+		for p1 in path.get_points():
+			p1 = path.get_point_position(p1)
+			# loop through the remaining nodes
+			for p2 in nodes:
+				if p1.distance_to(p2) < min_dist:
+					min_dist = p1.distance_to(p2)
+					min_p = p2
+					p = p1
+		var n = path.get_available_point_id()
+		path.add_point(n, min_p)
+		path.connect_points(path.get_closest_point(p), n)
+		nodes.erase(min_p)
+	return path
+	
+func make_map():
+	# create a tilemap from the generated rooms and path
+	Map.clear()
+	
+	# Fill tlemap with walls then carve empty rooms
+	var full_rect = Rect2()
+	for room in $Rooms.get_children():
+		var r = Rect2(room.position-room.size,
+				room.get_node("CollisionShape2D").shape.extents*2)
+		full_rect = full_rect.merge(r)
+	var topleft = Map.world_to_map(full_rect.position)
+	var bottomright = Map.world_to_map(full_rect.end)
+	for x in range(topleft.x, bottomright.x):
+		for y in range(topleft.y, bottomright.y):
+			Map.set_cell(x, y, 0)
+	
+	# Crave rooms
+	for room in $Rooms.get_children():
+		var s = (room.size / tile_size).floor()
+		var pos = Map.world_to_map(room.position)
+		var ul = (room.position / tile_size).floor() - s
+		for x in range(2, s.x * 2 - 1):
+			for y in range(2, s.y * 2 - 1):
+				Map.set_cell(ul.x + x, ul.y + y, 18)
+				if y == 2: # top of the room
+					Map.set_cell(ul.x + x, ul.y + y, 2)
+					if x == 2: # left corner
+						Map.set_cell(ul.x + x, ul.y + y, 1)
+					if x == s.x*2-2: # right corner
+						Map.set_cell(ul.x + x, ul.y + y, 6)	
+				if y == s.y*2-2: # bottom of the room
+					Map.set_cell(ul.x + x, ul.y + y, 34)
+					if x == 2: # left corner
+						Map.set_cell(ul.x + x, ul.y + y, 33)
+					if x == s.x*2-2: # right corner
+						Map.set_cell(ul.x + x, ul.y + y, 38)
+				if x == 2: # left wall
+					if y > 2 && y < s.y*2-2:
+						Map.set_cell(ul.x + x, ul.y + y, 9)
+				if x == s.x*2-2:
+					if y > 2 && y < s.y*2-2:
+						Map.set_cell(ul.x + x, ul.y + y, 14)
+	
+	# crave corridors
+	var corridors = []
+	for room in $Rooms.get_children():
+		# Carve connecting corridor
+		var p = path.get_closest_point(Vector3(room.position.x, 
+											room.position.y, 0))
+		for conn in path.get_point_connections(p):
+			if not conn in corridors:
+				var start = Map.world_to_map(Vector2(path.get_point_position(p).x,
+													path.get_point_position(p).y))
+				var end = Map.world_to_map(Vector2(path.get_point_position(conn).x,
+													path.get_point_position(conn).y))									
+				carve_path(start, end)
+		corridors.append(p)
+
+func carve_path(pos1, pos2):
+	#  carve a path between two points
+	var x_diff = sign(pos2.x - pos1.x)
+	var y_diff = sign(pos2.y - pos1.y)
+	if x_diff == 0: x_diff = pow(-1.0, randi() % 2)
+	if y_diff == 0: y_diff = pow(-1.0, randi() % 2)
+	# choose either do 
+	var x_y = pos1
+	var y_x = pos2
+	var rrr = (randi() % 2) > 0
+	if rrr:
+		x_y = pos2
+		y_x = pos1
+	for x in range(pos1.x, pos2.x + x_diff, x_diff):
+		if Map.get_cell(x, x_y.y) in [0, 1, 6]:
+			Map.set_cell(x, x_y.y, 24) # corredor
+			# if cell to the left is
+			if Map.get_cell(x - 1, x_y.y) in [33, 38]: # |_ or _|
+				Map.set_cell(x - 1, x_y.y, 24) # replace the cell with corridor
+			elif Map.get_cell(x - 1, x_y.y) in [9, 14]: # | : or : |
+				Map.set_cell(x - 1, x_y.y, 10) # replace the cell with cross road
+			elif Map.get_cell(x - 1, x_y.y) in [4]: # | |
+				Map.set_cell(x - 1, x_y.y, 9) # replace the cell with left wall only
+			
+			# if the cell to the right
+			if Map.get_cell(x + 1, x_y.y) in [33, 38]: # |_ or _|
+				Map.set_cell(x + 1, x_y.y, 24) # replace the cell with corridor
+			elif Map.get_cell(x + 1, x_y.y) in [9, 14]: # | : or : |
+				Map.set_cell(x + 1, x_y.y, 10) # replace the cell with cross road
+			elif Map.get_cell(x + 1, x_y.y) in [4]: # | |
+				Map.set_cell(x + 1, x_y.y, 14) # replace the cell with right wall only
+			
+			if Map.get_cell(x + 1, x_y.y) in [0]:
+				Map.set_cell(x, x_y.y, 38) # bottom right corner
+			elif Map.get_cell(x - 1, x_y.y) in [0]:
+				Map.set_cell(x, x_y.y, 33) # bottom left corner
+		elif Map.get_cell(x, x_y.y) in [2]:
+			Map.set_cell(x, x_y.y, 10) # chão
+		if Map.get_cell(x, x_y.y - 1) == 0:
+			Map.set_cell(x, x_y.y - 1, 2) # parede
+		elif Map.get_cell(x, x_y.y - 1) in [24, 34, 33, 38]:
+			Map.set_cell(x, x_y.y - 1, 10) # chão
+			
+	
+	# corredores verticais
+	var lsty
+	for y in range(pos1.y, pos2.y + y_diff, y_diff):
+		lsty = y
+		if Map.get_cell(y_x.x, y) in [0, 1, 2, 6]:
+			Map.set_cell(y_x.x, y, 4)
+			# if the cell to the top is
+			if Map.get_cell(y_x.x, y - 1) in [33]: # |_
+				Map.set_cell(y_x.x, y - 1, 9) # replace with | :
+			elif Map.get_cell(y_x.x, y - 1) in [38]: # _|
+				Map.set_cell(y_x.x, y - 1, 14) # replace with : |
+			elif Map.get_cell(y_x.x, y - 1) in [24]: # _
+				if Map.get_cell(y_x.x - 1, y - 1) in [24] and Map.get_cell(y_x.x + 1, y - 1) in [24]: # if its a ___
+					Map.set_cell(y_x.x, y - 1, 10) # replace with . .
+				elif Map.get_cell(y_x.x - 1, y - 1) in [24]: # if its a  __
+					Map.set_cell(y_x.x, y - 1, 14) # replace with | :
+				elif Map.get_cell(y_x.x + 1, y - 1) in [24]: # if its a  __
+					Map.set_cell(y_x.x, y - 1, 9) # replace with : |
+			elif Map.get_cell(y_x.x, y - 1) in [34]: # begin of room
+				Map.set_cell(y_x.x, y - 1, 10) # replace with ground
+	lsty = lsty - 1
+	if Map.get_cell(y_x.x, lsty + 1) in [24]: # if next cell is end of corridor
+		if not Map.get_cell(y_x.x + 1, lsty + 1) in [24] and Map.get_cell(y_x.x - 1, lsty + 1) in [24]:
+			Map.set_cell(y_x.x, lsty + 1, 38) # replace with _|
+		elif Map.get_cell(y_x.x + 1, lsty + 1) in [24] and not  Map.get_cell(y_x.x - 1, lsty + 1) in [24]:
+			Map.set_cell(y_x.x, lsty + 1, 33) # replace with |_
